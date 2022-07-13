1b5a939258b2f6c93efcbb11fa44acf2b39499d7
Jason <jasonkngjunhui@hotmail.sg>
2022-07-13 11:43:09 +0800

    Fixed secure route

--- src/app/shared/interceptor/api.interceptor.ts
+++ src/app/shared/interceptor/api.interceptor.ts
@@ -14,7 +14,7 @@ export class APIInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     console.log(request.url)
-    const apiReq = request.clone({url: `http://football-jason.herokuapp.com/${request.url}`})
+    const apiReq = request.clone({url: `https://football-jason.herokuapp.com/${request.url}`})
     // const apiReq = request.clone({url: `http://localhost:5000/${request.url}`})
     return next.handle(apiReq);
   }
