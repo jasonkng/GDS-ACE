8441aa7b24a8be7d0ddcb61adaaa126bee28c208
Jason <jasonkngjunhui@hotmail.sg>
2022-07-13 12:01:32 +0800

    configure interceptor for local and prod env

--- src/app/shared/interceptor/api.interceptor.ts
+++ src/app/shared/interceptor/api.interceptor.ts
@@ -6,6 +6,7 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
@@ -13,9 +14,9 @@ export class APIInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    console.log(request.url)
-    const apiReq = request.clone({url: `https://football-jason.herokuapp.com/${request.url}`})
-    // const apiReq = request.clone({url: `http://localhost:5000/${request.url}`})
+    console.log(environment.production);
+    const apiReq = environment.production ? request.clone({url: `https://football-jason.herokuapp.com/${request.url}`}) : 
+    request.clone({url: `http://localhost:5000/${request.url}`})
     return next.handle(apiReq);
   }
 }
